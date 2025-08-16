dc4201197166bf0778b3e4f404748db5d9bfc960
Emad Albalas <emadalbalas@gmail.com>
2025-08-15 23:48:06 -0700

    updated config to include function, and boot

--- keymap.c
+++ keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, KC_F2,   KC_F5,   KC_F8,   KC_F11, XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KQ_BOOT, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, KC_F3,   KC_F6,   KC_F9,   KC_F12, XXXXXXX,
+     QK_BOOT, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, KC_F3,   KC_F6,   KC_F9,   KC_F12, XXXXXXX,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX,  XXXXXXX,XXXXXXX
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
